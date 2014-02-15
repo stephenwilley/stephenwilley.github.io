d157bf0e892a0c9b1f5001afef3a609c0a1575c3
Stephen Willey <stephen@esstec.co.uk>
2014-02-15 00:36:00 +0000

    Make it more red

--- shaders/undulating-f.glsl
+++ shaders/undulating-f.glsl
@@ -10,5 +10,5 @@ void main()
 	vec3 light = vec3(0.5, 0.2, 1.0);
 	light = normalize(light);
 	float dProd = max(0.0, dot(vNormal, light));
-	gl_FragColor = vec4(dProd, dProd, dProd, 1.0);
+	gl_FragColor = vec4(dProd * 2.0, dProd / 2.0, dProd / 2.0 , 1.0);
 }
